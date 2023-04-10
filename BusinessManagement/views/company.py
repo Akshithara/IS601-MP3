54efc46e3bca468894cd0cfcf149221aee3fcb2c
Akshithara <123113065+Akshithara@users.noreply.github.com>
2023-04-11 00:49:21 +0530

    Add files via upload

--- BusinessManagement/views/company.py
+++ BusinessManagement/views/company.py
@@ -1,45 +1,84 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from sql.db import DB
+import pycountry
 company = Blueprint('company', __name__, url_prefix='/company')
+
 
 @company.route("/search", methods=["GET"])
 def search():
     rows = []
-    # DO NOT DELETE PROVIDED COMMENTS
-    # TODO search-1 retrieve id, name, address, city, country, state, zip, website, employee count as employees for the company
-    # don't do SELECT *
     
-    query = "... WHERE 1=1"
-    args = {} # <--- add values to replace %s/%(named)s placeholders
-    allowed_columns = ["name", "city", "country", "state"]
+    # TODO search-1 retrieve id, name, address, city, country, state, zip, website, employee count as employees for the company
+    query = """
+    SELECT companies.id, companies.name, companies.address, companies.city, companies.country, companies.state, companies.zip, companies.website, COUNT(employees.id) as employees 
+    FROM IS601_MP3_Companies companies LEFT JOIN IS601_MP3_Employees employees ON companies.id = employees.company_id 
+    WHERE 1=1
+    """
+    group_by_query = " GROUP BY companies.id, companies.name, companies.address, companies.city, companies.country, companies.state, companies.zip, companies.website"
+    
+    
+    args = {}
+    
+    allowed_columns = [("name", "Name"), ("address", "Address"), ("city", "City"), ("country", "Country"), ("state", "State"), ("zip", "Zip"), ("Website", "Website"), ("employees","Employees")]
+    
     # TODO search-2 get name, country, state, column, order, limit request args
+    name = request.args.get("name")
+    country = request.args.get("country")
+    state = request.args.get("state")
+    column = request.args.get("column")
+    order = request.args.get("order")
+    limit = request.args.get("limit")
+    
     # TODO search-3 append a LIKE filter for name if provided
+    if name:
+        name_filter = f"%{name}%"
+        query += " AND companies.name LIKE %(name_filter)s"
+        args["name_filter"] = name_filter
+    
     # TODO search-4 append an equality filter for country if provided
+    if country:
+        query += " AND companies.country = %(country)s"
+        args["country"] = country
+    
     # TODO search-5 append an equality filter for state if provided
-    # TODO search-6 append sorting if column and order are provided and within the allows columsn and allowed order asc,desc
-    # TODO search-7 append limit (default 10) or limit greater than 1 and less than or equal to 100
-    # TODO search-8 provide a proper error message if limit isn't a number or if it's out of bounds
+    if state:
+        query += " AND companies.state = %(state)s"
+        args["state"] = state
+    
+    query += group_by_query
+    
+    # TODO search-6 append sorting if column and order are provided and within the allowed columns and allowed order asc,desc
+    if column and order:
+        if column in [col[0] for col in allowed_columns] and order.lower() in ["asc", "desc"]:
+            query += f" ORDER BY {column} {order}"
+    
+    # TODO search-7 append limit (default 10) or limit greater than or equal to 1 and less than or equal to 100
+    if limit:
+        try:
+            limit = int(limit)
+            if limit < 1 or limit > 100:
+                raise ValueError
+        except ValueError:
+            flash("Limit must be a number between 1 and 100", "danger")
+        else:
+            query += " LIMIT %(limit)s"
+            args["limit"] = limit
+    else:
+        query += " LIMIT 10"
     
 
-    limit = 10 # TODO change this per the above requirements
-    query += " LIMIT %(limit)s"
-    args["limit"] = limit
     print("query",query)
     print("args", args)
     try:
         result = DB.selectAll(query, args)
-        #print(f"result {result.rows}")
         if result.status:
             rows = result.rows
-            print(f"rows {rows}")
     except Exception as e:
         # TODO search-9 make message user friendly
-        flash(str(e), "danger")
-    # hint: use allowed_columns in template to generate sort dropdown
-    # hint2: convert allowed_columns into a list of tuples representing (value, label)
-    # do this prior to passing to render_template, but not before otherwise it can break validation
+        flash("An error occurred while searching for Companies.", "danger")
     
     return render_template("list_companies.html", rows=rows, allowed_columns=allowed_columns)
+
 
 @company.route("/add", methods=["GET","POST"])
 def add():
@@ -58,31 +97,88 @@ def add():
         # TODO add-8 zipcode is required (flash proper error message)
         # note: call zip variable zipcode as zip is a built in function it could lead to issues
 
+        name = request.form.get("name")
+        address = request.form.get("address")
+        city = request.form.get("city")
+        state = request.form.get("state")
+        country = request.form.get("country")
+        zip_code = request.form.get("zip")
+        website = request.form.get("website")
+
         has_error = False # use this to control whether or not an insert occurs
         
+        if not name:
+            flash("Name is required", "danger")
+            has_error = True
+        
+        if not address:
+            flash("Address is required", "danger")
+            has_error = True
+        
+        if not city:
+            flash("City is required", "danger")
+            has_error = True
+        
+        if not state:
+            flash("State is required", "danger")
+            has_error = True
+        elif not is_valid_state(state, country):
+            flash("Invalid state selected", "danger")
+            has_error = True
+        
+        if not country:
+            flash("Country is required", "danger")
+            has_error = True
+        elif not is_valid_country(country):
+            flash("Invalid country selected", "danger")
+            has_error = True
+        
+        if not zip_code:
+            flash("Zipcode is required", "danger")
+            has_error = True
 
         if not has_error:
             try:
                 result = DB.insertOne("""
-                INSERT INTO ...
-                ...
-                VALUES
-                ...
-                """, ...) # <-- TODO add-8 add query and add arguments
+                INSERT INTO IS601_MP3_Companies (name, address, city, state, country, zip, website)
+                VALUES (%(name)s, %(address)s, %(city)s, %(state)s, %(country)s, %(zip)s, %(website)s);
+                """, {"name": name, "address": address, "city": city, "state": state, "country": country, "zip": zip_code, "website": website}) # <-- TODO add-8 add query and add arguments
                 if result.status:
-                    flash("Added Company", "success")
+                    # TODO add-11 Proper INSERT query should be visible along with the data being inserted
+                    flash("Added Company.", "success")
+                    return redirect(url_for("company.add"))
             except Exception as e:
                 # TODO add-9 make message user friendly
-                flash(str(e), "danger")
+                print(str(e))
+                flash("An exception occured while Adding the Company details!", "danger")
         
     return render_template("add_company.html")
+
+def is_valid_state(state_code, country_code):
+    try:
+        # country = pycountry.countries.get(alpha_2=country_code)
+        states = pycountry.subdivisions.get(country_code=country_code)
+        valid_states = [state.code for state in states]
+        if country_code + '-' + state_code in valid_states:
+            return True
+        return False
+    except (KeyError, AttributeError):
+        return False
+
+def is_valid_country(country_code):
+    try:
+        pycountry.countries.get(alpha_2=country_code)
+        return True
+    except KeyError:
+        return False
 
 @company.route("/edit", methods=["GET", "POST"])
 def edit():
     # TODO edit-1 request args id is required (flash proper error message)
-    id = False
+    id = request.args.get("id")
     if not id: # TODO update this for TODO edit-1
-        pass
+        flash("Company ID is required.", "danger")
+        return redirect(url_for("company.search"))
     else:
         if request.method == "POST":
             data = {"id": id} # use this as needed, can convert to tuple if necessary
@@ -98,20 +194,89 @@ def edit():
             # hint see geography.py and pycountry documentation
             # TODO edit-7 website is not required
             # TODO edit-8 zipcode is required (flash proper error message)
-            
-            # note: call zip variable zipcode as zip is a built in function it could lead to issues
-            # populate data dict with mappings
+            name = request.form.get("name")
+            address = request.form.get("address")
+            city = request.form.get("city")
+            state = request.form.get("state")
+            country = request.form.get("country")
+            zip_code = request.form.get("zip")
+            website = request.form.get("website")
+
             has_error = False # use this to control whether or not an insert occurs
 
+            # TODO edit-2 name is required (flash proper error message)
+            if not name:
+                flash("Name is required", "danger")
+                has_error = True
+            else:
+                data["name"] = name
             
+            # TODO edit-3 address is required (flash proper error message)
+            if not address:
+                flash("Address is required", "danger")
+                has_error = True
+            else:
+                data["address"] = address
+            
+            # TODO edit-4 city is required (flash proper error message)
+            if not city:
+                flash("City is required", "danger")
+                has_error = True
+            else:
+                data["city"] = city
+            
+            if not state:
+                flash("State is required", "danger")
+                has_error = True
+            elif not is_valid_state(state, country):
+                flash("Invalid state selected", "danger")
+                has_error = True
+            else:
+                state = pycountry.subdivisions.lookup(country +'-'+state)
+                data["state"] = state.code.split('-')[1]
+                
+            # TODO edit-6 country is required (flash proper error message)
+            if not country:
+                flash("Country is required", "danger")
+                has_error = True
+            elif not is_valid_country(country):
+                flash("Invalid country selected", "danger")
+                has_error = True
+            else:
+                country = pycountry.countries.search_fuzzy(country)[0]
+                print(country)
+                data["country"] = country.alpha_2
+            
+            data["website"] = website
+        
+            # TODO edit-8 zipcode is required (flash proper error message)
+            if not zip_code:
+                flash("Zipcode is required", "danger")
+                has_error = True
+            else:
+                data["zip"] = zip_code
+            
+
+            # note: call zip variable zipcode as zip is a built in function it could lead to issues
+            # populate data dict with mappings
+            
+            print(data)
             if not has_error:
                 try:
                     # TODO edit-9 fill in proper update query
                     # name, address, city, state, country, zip, website
                     result = DB.update("""
-                    UPDATE ...
+                    UPDATE IS601_MP3_Companies
                     SET
-                    ...
+                        name=%(name)s,
+                        address=%(address)s,
+                        city=%(city)s,
+                        state=%(state)s,
+                        country=%(country)s,
+                        zip=%(zip)s,
+                        website=%(website)s
+                    WHERE
+                        id=%(id)s
                     """, data)
                     if result.status:
                         print("updated record")
@@ -119,19 +284,19 @@ def edit():
                 except Exception as e:
                     # TODO edit-10 make this user-friendly
                     print(f"{e}")
-                    flash(str(e), "danger")
+                    flash("An error occurred while updating the Company record.", "danger")
         row = {}
         try:
             # TODO edit-11 fetch the updated data
-            result = DB.selectOne("SELECT ... FROM ... WHERE ...", id)
+            result = DB.selectOne("SELECT * FROM IS601_MP3_Companies WHERE id=%s", id)
             if result.status:
                 row = result.row
                 
         except Exception as e:
             # TODO edit-12 make this user-friendly
-            flash(str(e), "danger")
+            flash("An error occurred while fetching the company record.", "danger")
     # TODO edit-13 pass the company data to the render template
-    return render_template("edit_company.html", ...)
+    return render_template("edit_company.html", row=row)
 
 @company.route("/delete", methods=["GET"])
 def delete():
@@ -141,5 +306,23 @@ def delete():
     # TODO delete-4 ensure a flash message shows for successful delete
     # TODO delete-5 for all employees assigned to this company set their company_id to None/null
     # TODO delete-6 if id is missing, flash necessary message and redirect to search
-    pass
+    id = request.args.get("id")
+    if id:
+        try:
+            DB.update("UPDATE IS601_MP3_Employees SET company_id = NULL WHERE company_id = %(id)s", {"id": id})
+            result = DB.delete("DELETE FROM IS601_MP3_Companies WHERE id = %(id)s;", {"id": id})
+            if result.status:
+                flash("Successfully deleted company.", "success")
+        except Exception as e:
+            flash("An error occurred while deleting the company record.", "danger")
+            print(e)
+
+        # TODO delete-3 pass all arguments except id to this route
+        args = {k: v for k, v in request.args.items() if k != "id"}
+        # TODO delete-2 redirect to employee search
+        return redirect(url_for("company.search", **args))
+    else:
+        # TODO delete-5 if id is missing, flash necessary message and redirect to search
+        flash("Company ID is required.", "danger")
+        return redirect(url_for("company.search"))
     
--- BusinessManagement/views/employee.py
+++ BusinessManagement/views/employee.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, render_template, request, flash, url_for
 from sql.db import DB
+import re
 employee = Blueprint('employee', __name__, url_prefix='/employee')
 
 
@@ -8,20 +9,57 @@ def search():
     rows = []
     # DO NOT DELETE PROVIDED COMMENTS
     # TODO search-1 retrieve employee id as id, first_name, last_name, email, company_id, company_name using a LEFT JOIN
-    query = """SELECT ...
-     FROM ... LEFT JOIN ... WHERE 1=1"""
+    query = """SELECT e.id, e.first_name, e.last_name, e.email, e.company_id, c.name
+     FROM IS601_MP3_Employees e LEFT JOIN IS601_MP3_Companies c ON e.company_id = c.id WHERE 1=1"""
     args = {} # <--- add values to replace %s/%(named)s placeholders
-    allowed_columns = ["first_name", "last_name", "email", "company_name"]
-    # TODO search-2 get fn, ln, email, company, column, order, limit from request args
-    # TODO search-3 append like filter for first_name if provided
-    # TODO search-4 append like filter for last_name if provided
-    # TODO search-5 append like filter for email if provided
-    # TODO search-6 append equality filter for company_id if provided
-    # TODO search-7 append sorting if column and order are provided and within the allowed columns and order options (asc, desc)
-    # TODO search-8 append limit (default 10) or limit greater than 1 and less than or equal to 100
-    # TODO search-9 provide a proper error message if limit isn't a number or if it's out of bounds
+    allowed_columns = [("first_name", "First Name"), ("last_name", "Last Name"), ("email", "Email"), ("company_id", "Company Id"), ("name", "Company Name")]
 
-    limit = 10 # TODO change this per the above requirements
+    # TODO search-2 get fn, ln, email, company, column, order, limit from request args
+    fn = request.args.get('fn')
+    ln = request.args.get('ln')
+    email = request.args.get('email')
+    company = request.args.get('company')
+    column = request.args.get('column')
+    order = request.args.get('order')
+    limit = request.args.get('limit')
+
+    # TODO search-3 append like filter for first_name if provided
+    if fn:
+        query += " AND e.first_name LIKE %(fn)s"
+        args["fn"] = f"%{fn}%"
+
+    # TODO search-4 append like filter for last_name if provided
+    if ln:
+        query += " AND e.last_name LIKE %(ln)s"
+        args["ln"] = f"%{ln}%"
+
+    # TODO search-5 append like filter for email if provided
+    if email:
+        query += " AND e.email LIKE %(email)s"
+        args["email"] = f"%{email}%"
+
+    # TODO search-6 append equality filter for company_id if provided
+    if company:
+        query += " AND e.company_id = %(company)s"
+        args["company"] = company
+
+    # TODO search-7 append sorting if column and order are provided and within the allowed columns and order options (asc, desc)
+    if column and order and column in [x[0] for x in allowed_columns] and order in ["asc", "desc"]:
+        query += f" ORDER BY {column} {order.upper()}"
+
+    # TODO search-8 append limit (default 10) or limit greater than 1 and less than or equal to 100
+    if limit:
+        try:
+            limit = int(limit)
+            if limit < 1 or limit > 100:
+                raise ValueError
+        except ValueError:
+            # TODO search-9 provide a proper error message if limit isn't a number or if it's out of bounds
+            flash("Limit must be a number between 1 and 100", "error")
+            limit = 10
+    else:
+        limit = 10
+
     query += " LIMIT %(limit)s"
     args["limit"] = limit
     print("query",query)
@@ -32,11 +70,7 @@ def search():
             rows = result.rows
     except Exception as e:
         # TODO search-10 make message user friendly
-        flash(e, "error")
-    # hint: use allowed_columns in template to generate sort dropdown
-    # hint2: convert allowed_columns into a list of tuples representing (value, label)
-    # do this prior to passing to render_template, but not before otherwise it can break validation
-   
+        flash("An error occurred while searching for employees.", "danger")   
     return render_template("list_employees.html", rows=rows, allowed_columns=allowed_columns)
 
 @employee.route("/add", methods=["GET","POST"])
@@ -49,73 +83,151 @@ def add():
         # TODO add-5 email is required (flash proper error message)
         # TODO add-5a verify email is in the correct format
         has_error = False # use this to control whether or not an insert occurs
-            
+        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        company = request.form.get('company')
+        email = request.form.get('email')
+
+        # Validate required fields
+        if not first_name:
+            flash('First name is required.', 'warning')
+            has_error = True
+        if not last_name:
+            flash('Last name is required.', 'warning')
+            has_error = True
+        if not email:
+            flash('Email is required.', 'warning')
+            has_error = True
+        elif not re.match(email_pattern, email):
+            flash('Email is not in the correct format.', 'warning')
+            has_error = True
+        
+        args = {
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'company_name': company
+        }
+
         if not has_error:
             try:
                 result = DB.insertOne("""
-                INSERT INTO ...
-                """, ...
-                ) # <-- TODO add-6 add query and add arguments
+                INSERT INTO IS601_MP3_Employees (first_name, last_name, email, company_id)
+                    VALUES (%(first_name)s, %(last_name)s, %(email)s, (SELECT id FROM IS601_MP3_Companies WHERE name = %(company_name)s LIMIT 1))
+                    ON DUPLICATE KEY UPDATE first_name=%(first_name)s, 
+                    last_name = %(last_name)s, email = %(email)s, 
+                    company_id = (SELECT id FROM IS601_MP3_Companies WHERE name=%(company_name)s LIMIT 1)
+                """, args
+                ) # <-- DONE - TODO add-6 add query and add arguments
                 if result.status:
                     flash("Created Employee Record", "success")
             except Exception as e:
                 # TODO add-7 make message user friendly
-                flash(str(e), "danger")
+                print(str(e))
+                flash("An exception occured while Adding the Employee details!", "danger")
     return render_template("add_employee.html")
+
 
 @employee.route("/edit", methods=["GET", "POST"])
 def edit():
     # TODO edit-1 request args id is required (flash proper error message)
-    id = False
-    if not id: # TODO update this for TODO edit-1
-        pass
+    id = request.args.get("id")
+    if not id:
+        flash("Employee ID is required.", "danger")
+        return redirect(url_for("employee.search"))
     else:
         if request.method == "POST":
-            
             # TODO edit-1 retrieve form data for first_name, last_name, company, email
-            # TODO edit-2 first_name is required (flash proper error message)
-            # TODO edit-3 last_name is required (flash proper error message)
-            # TODO edit-4 company (may be None)
-            # TODO edit-5 email is required (flash proper error message)
-            # TODO edit-5a verify email is in the correct format
-            has_error = False # use this to control whether or not an insert occurs
+            first_name = request.form.get("first_name")
+            last_name = request.form.get("last_name")
+            company_id = request.form.get("company")
+            email = request.form.get("email")
 
-            
-                
+            email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+            has_error = False
+            # TODO edit-2 first_name is required (flash proper error message)
+            if not first_name:
+                flash("First name is required.", "danger")
+                has_error = True
+            # TODO edit-3 last_name is required (flash proper error message)
+            if not last_name:
+                flash("Last name is required.", "danger")
+                has_error = True
+            # TODO edit-4 company (may be None)
+            # company_id will be None if no value was selected in the form
+            # we don't need to do any validation for this field
+            # TODO edit-5 email is required (flash proper error message)
+            if not email:
+                flash("Email is required.", "danger")
+                has_error = True
+            else:
+                # TODO edit-5a verify email is in the correct format
+                if not re.match(email_pattern, email):
+                    flash("Invalid email format.", "danger")
+                    has_error = True
+
             if not has_error:
                 try:
                     # TODO edit-6 fill in proper update query
                     result = DB.update("""
-                    UPDATE ... SET
-                    ...
-                    """, ...)
+                    UPDATE IS601_MP3_Employees SET
+                    first_name=%(first_name)s,
+                    last_name=%(last_name)s,
+                    company_id=%(company_id)s,
+                    email=%(email)s
+                    WHERE id=%(id)s;
+                    """, {"first_name": first_name, "last_name": last_name, "company_id": company_id, "email": email, "id": id})
                     if result.status:
                         flash("Updated record", "success")
                 except Exception as e:
                     # TODO edit-7 make this user-friendly
-                    flash(e, "danger")
+                    flash("An error occurred while updating the employee record.", "danger")
+                    print(e)
+
         row = {}
         try:
-            # TODO edit-8 fetch the updated data 
-            result = DB.selectOne("""SELECT 
-            ...
-            FROM ... LEFT JOIN ... 
-              
-              WHERE ..."""
-            , id)
+            # TODO edit-8 fetch the updated data
+            result = DB.selectOne("""SELECT
+            IS601_MP3_Employees.id AS id,
+            IS601_MP3_Employees.first_name AS first_name,
+            IS601_MP3_Employees.last_name AS last_name,
+            IS601_MP3_Employees.email AS email,
+            IS601_MP3_Employees.company_id AS company_id
+            FROM IS601_MP3_Employees LEFT JOIN IS601_MP3_Companies
+            ON IS601_MP3_Employees.company_id = IS601_MP3_Companies.id
+            WHERE IS601_MP3_Employees.id =  %(id)s;""", {"id": id})
             if result.status:
                 row = result.row
         except Exception as e:
             # TODO edit-9 make this user-friendly
-            flash(str(e), "danger")
+            flash("An error occurred while fetching the employee record.", "danger")
+            print(e)
+
     # TODO edit-10 pass the employee data to the render template
-    return render_template("edit_employee.html", ...)
+    return render_template("edit_employee.html", row=row)#, company_dropdown=company_dropdown)
+
 
 @employee.route("/delete", methods=["GET"])
 def delete():
     # TODO delete-1 delete employee by id
-    # TODO delete-2 redirect to employee search
-    # TODO delete-3 pass all argument except id to this route
-    # TODO delete-4 ensure a flash message shows for successful delete
-    # TODO delete-5 if id is missing, flash necessary message and redirect to search
-    pass
+    id = request.args.get("id")
+    if id:
+        try:
+            result = DB.delete("DELETE FROM IS601_MP3_Employees WHERE id = %(id)s;", {"id": id})
+            if result.status:
+                flash("Successfully deleted employee.", "success")
+        except Exception as e:
+            flash("An error occurred while deleting the employee record.", "danger")
+            print(e)
+
+        # TODO delete-3 pass all arguments except id to this route
+        args = {k: v for k, v in request.args.items() if k != "id"}
+        # TODO delete-2 redirect to employee search
+        return redirect(url_for("employee.search", **args))
+    else:
+        # TODO delete-5 if id is missing, flash necessary message and redirect to search
+        flash("Employee ID is required.", "danger")
+        return redirect(url_for("employee.search"))
